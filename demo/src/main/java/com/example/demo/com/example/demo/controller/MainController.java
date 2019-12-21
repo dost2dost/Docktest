cee42de85fbc0c8db37b1749e857f3e94e4924b1
dost2dost <dost2dost@gmail.com>
2019-12-22 01:47:23 +0500

    controller is added

--- demo/src/main/java/com/example/demo/com/example/demo/controller/MainController.java
+++ demo/src/main/java/com/example/demo/com/example/demo/controller/MainController.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.example.demo.com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MainController {
     @GetMapping(value = "/test")
-    
     public String disp(){
         return "hello from controller ";
     }
